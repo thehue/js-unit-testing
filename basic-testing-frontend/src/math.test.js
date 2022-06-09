446d5bb328f361826f9cc58eba427e4e16883bde
thehue <gouk32@naver.com>
2022-06-09 22:26:09 +0900

    AAA pattern test
    테스트는 최대한 간단해야 한다

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,6 +3,16 @@ import { expect, it } from 'vitest';
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange
+  const numbers = [1, 2];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0,
+  );
+  expect(result).toBe(expectedResult);
 });
